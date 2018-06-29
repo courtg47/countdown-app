0bbeedb334f3b6de1469a7b11b645098dcbfbdc8
Courtney Green <courtg47@gmail.com>
2018-06-29 16:19:27 -0500

    feat: Introduced state into the countdown App
    Added a constructor in the App class which created a state for the
    countdown date.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadLine: 'December 25, 2018'
+    };
+  }
+
   render() {
     return (
       <section className="App">
-        <h1 className="App-title">Countdown to Christmas: December 25, 2018</h1>
+        <h1 className="App-title">Countdown to Christmas: {this.state.deadLine}</h1>
         <section>
           <section className="days">14 days</section>
           <section className="hours">30 hours</section>
