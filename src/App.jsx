b592bba32086a286157da5eac3da7abc5c024c3a
Courtney Green <courtg47@gmail.com>
2018-06-29 15:54:38 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Countdown to Christmas: December 25, 2018</h1>
+        <section>
+          <section>14 days</section>
+          <section>30 hours</section>
+          <section>15 minutes</section>
+          <section>20 seconds</section>
+        </section>
+      </section>
+    )
+  }
+}
+
+export default App;
