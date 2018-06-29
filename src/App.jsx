f6cf1d21ad5218241f3363e1a55c7328fb182532
Courtney Green <courtg47@gmail.com>
2018-06-29 16:10:31 -0500

    style: Add basic styling to Countdown App
    Added basic styling to the title and different components of the clock.
    Styling mostly consisted of margin, spacing, and font size. The
    App.css file was also initialized in this commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <section>
-        <h1>Countdown to Christmas: December 25, 2018</h1>
+      <section className="App">
+        <h1 className="App-title">Countdown to Christmas: December 25, 2018</h1>
         <section>
-          <section>14 days</section>
-          <section>30 hours</section>
-          <section>15 minutes</section>
-          <section>20 seconds</section>
+          <section className="days">14 days</section>
+          <section className="hours">30 hours</section>
+          <section className="minutes">15 minutes</section>
+          <section className="seconds">20 seconds</section>
+        </section>
+        <section>
+          <input placeholder="new date" />
+          <button>Submit</button>
         </section>
       </section>
     )
