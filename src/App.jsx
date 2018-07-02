8aebc9f6c323ffbf6c66e94682ecc216ae3081d7
Courtney Green <courtg47@gmail.com>
2018-07-01 22:39:41 -0500

    style: Add React Bootstrap for responsiveness and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Clock from './Clock'
+import Clock from './Clock';
 import './App.css';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props) {
@@ -21,10 +22,10 @@ class App extends Component {
       <section className="App">
         <h1 className="App-title">Countdown to Christmas: {this.state.deadLine}</h1>
         <Clock deadLine={this.state.deadLine}/>
-        <section>
-          <input onChange={event => this.setState({newDeadline: event.target.value})} placeholder="new date" />
-          <button onClick={() => this.changeDeadline()}>Submit</button>
-        </section>
+        <Form inline>
+          <FormControl className="deadline-input" onChange={event => this.setState({newDeadline: event.target.value})} placeholder="new date" />
+          <Button onClick={() => this.changeDeadline()}>Submit</Button>
+        </Form>
       </section>
     )
   }
